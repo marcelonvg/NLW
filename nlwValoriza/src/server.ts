732b7f59fc6fd3181817dc1e3956290a049006d3
MarceloNvG <marcelonvgdev@gmail.com>
2021-06-22 18:10:33 -0300

    Aula02 31:49/1:42:41
    Maximum Speed

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,23 +1,8 @@
-import express, { response } from "express";
+import "reflect-metadata";
+import express from "express";
+import "./database";
+
 
 const app = express();
-
-/*
-* GET    => Buscar uma informacao
-* POST   => Inserir (Criar) uma informacao
-* PUT    => Alterar uma informacao
-* DELETE => Remover um dado
-* PATCH  => Alterar uma informacao especifica
-*/
-
-app.get("/test", (request, response) =>{
-// Request  = Entrando
-// Response = Saindo
-return response.send("Olá NLW");
-});
-
-app.post("/test-post", (resquest, response) => {
-return response.send("Olá NLW método POST");
-});
 
 app.listen(3000, () => console.log("Server is running"));
